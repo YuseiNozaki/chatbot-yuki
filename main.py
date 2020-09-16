700ae76a73addb3292ec58d91b88247883586846
YuseiNozaki <Yusei.9933@gmail.com>
2020-09-16 12:29:41 +0900

    update main

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def callback():
 
 @handoler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好き♡'))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
 
